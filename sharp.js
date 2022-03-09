02a2e87e5aa4e6d7d8bae77dcada81098b165c2a
Neeraj Dalal <nd941z@gmail.com>
2022-03-09 22:57:27 +0530

    added sharp test for fun

--- sharp.js
+++ sharp.js
@@ -9,7 +9,7 @@ const run = async () => {
   console.log(await image.metadata())
 
   image.toFormat('jpeg', { quality: 80 }).toFile('./sharp/sharp-80.jpeg')
-  image.toFormat('jpeg', { mozjpeg: true, quality: 80 }).toFile('./sharp/sharp-80-moz.jpeg')
+  image.toFormat('jpeg', { quality: 80, mozjpeg: true }).toFile('./sharp/sharp-80-moz.jpeg')
   image.toFormat('png', { quality: 80 }).toFile('./sharp/sharp-80.png')
   image.toFormat('webp', { quality: 80 }).toFile('./sharp/sharp-80.webp')
   image.toFormat('webp', { quality: 80, effort: 6 }).toFile('./sharp/sharp-80-e6.webp')

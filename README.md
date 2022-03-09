6389b745d794e5e1634a9d8e8be1a9d44349f076
Neeraj Dalal <nd941z@gmail.com>
2022-03-09 23:18:44 +0530

    added sharp test for fun

--- README.md
+++ README.md
@@ -5,9 +5,15 @@ To get API_ID and API_HASH visit https://my.telegram.org/auth?to=apps
 ### Results by Sharp
 
 ```
+496K    sharp/alpha-80-e6.webp
+496K    sharp/alpha-80-moz.jpeg
+516K    sharp/alpha-80.webp
+580K    sharp/alpha-80.jpeg
 3.3M    sharp/alpha.jpg
-2.1M    sharp/alpha-80-e6.webp
-2.2M    sharp/alpha-80.webp
-2.6M    sharp/alpha-80.jpeg
-3.1M    sharp/alpha-80-moz.jpeg
+
+100K    sharp/beta-80-e6.webp
+104K    sharp/beta-80.webp
+156K    sharp/beta-80-moz.jpeg
+192K    sharp/beta-80.jpeg
+1.4M    sharp/beta.jpg
 ```
--- sharp.js
+++ sharp.js
@@ -1,12 +1,17 @@
 import sharp from 'sharp'
 
 const run = async () => {
-  const image = await sharp('./sharp/alpha.jpg')
+  const image = await sharp('./sharp/beta.jpg').resize({
+    width: 1920,
+    height: 1080,
+  })
 
-  image.toFormat('jpeg', { quality: 80 }).toFile('./sharp/sharp-80.jpeg')
-  image.toFormat('jpeg', { quality: 80, mozjpeg: true }).toFile('./sharp/sharp-80-moz.jpeg')
-  image.toFormat('webp', { quality: 80 }).toFile('./sharp/sharp-80.webp')
-  image.toFormat('webp', { quality: 80, effort: 6 }).toFile('./sharp/sharp-80-e6.webp')
+  console.log(await image.metadata())
+
+  image.toFormat('jpeg', { quality: 80 }).toFile('./sharp/beta-80.jpeg')
+  image.toFormat('jpeg', { quality: 80, mozjpeg: true }).toFile('./sharp/beta-80-moz.jpeg')
+  image.toFormat('webp', { quality: 80 }).toFile('./sharp/beta-80.webp')
+  image.toFormat('webp', { quality: 80, effort: 6 }).toFile('./sharp/beta-80-e6.webp')
 }
 
 run()
